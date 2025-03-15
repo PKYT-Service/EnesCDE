657beed4dfaffc10d8fd0348307f4b86726f8ff4
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-03-15 22:07:27 +0100

    Update __menu__.js

--- users/panel/__menu__.js
+++ users/panel/__menu__.js
@@ -26,20 +26,22 @@ document.addEventListener("DOMContentLoaded", function () {
     <a class="text-red-400 dar:text-red-950">Edite Profil</a>
   </button>
   </a>
-    <a href="./delete_account.html">
+    <button class="inline-block rounded-md px-4 py-2 text-sm text-gray-500 hover:text-gray-700 focus:relative dark:text-gray-400 dark:hover:text-gray-200"></button>
+<a href="./delete_account.html">
   <button
     class="inline-block rounded-md bg-white px-4 py-2 text-sm text-blue-500 shadow-xs focus:relative dark:bg-gray-800"
   >
     <a class="text-red-400 dar:text-red-950">Delete Profil</a>
   </button>
-  </a>
-    <a href="./logout.html">
+</a>
+    <button class="inline-block rounded-md px-4 py-2 text-sm text-gray-500 hover:text-gray-700 focus:relative dark:text-gray-400 dark:hover:text-gray-200"></button>
+<a href="./logout.html">
   <button
     class="inline-block rounded-md bg-white px-4 py-2 text-sm text-blue-500 shadow-xs focus:relative dark:bg-gray-800"
   >
     Déconnexion
   </button>
-  </a>
+</a>
 </div>
 
         
