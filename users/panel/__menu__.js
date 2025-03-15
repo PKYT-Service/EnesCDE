225f5617af54cb45359620f7c938548be5d03845
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-03-15 22:06:18 +0100

    Update __menu__.js

--- users/panel/__menu__.js
+++ users/panel/__menu__.js
@@ -2,23 +2,44 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("ecde_users_menu").innerHTML = `     
 
 <div class="inline-flex rounded-lg border p-1 bg-gray-100 dark:bg-gray-900 dark:border-gray-800">
+
+  <a href="./index.html">
   <button
     class="inline-block rounded-md px-4 py-2 text-sm text-gray-500 hover:text-gray-700 focus:relative dark:text-gray-400 dark:hover:text-gray-200"
   >
-    Edit
+    Home
   </button>
-
+  </a>
+  
+  <a href="./Profil.html">
   <button
     class="inline-block rounded-md px-4 py-2 text-sm text-gray-500 hover:text-gray-700 focus:relative dark:text-gray-400 dark:hover:text-gray-200"
   >
-    View
+    Profil
   </button>
-
+  </a>
+  
+  <a href="./edite_account.html">
   <button
     class="inline-block rounded-md bg-white px-4 py-2 text-sm text-blue-500 shadow-xs focus:relative dark:bg-gray-800"
   >
-    Delete
+    <a class="text-red-400 dar:text-red-950">Edite Profil</a>
   </button>
+  </a>
+    <a href="./delete_account.html">
+  <button
+    class="inline-block rounded-md bg-white px-4 py-2 text-sm text-blue-500 shadow-xs focus:relative dark:bg-gray-800"
+  >
+    <a class="text-red-400 dar:text-red-950">Delete Profil</a>
+  </button>
+  </a>
+    <a href="./logout.html">
+  <button
+    class="inline-block rounded-md bg-white px-4 py-2 text-sm text-blue-500 shadow-xs focus:relative dark:bg-gray-800"
+  >
+    Déconnexion
+  </button>
+  </a>
 </div>
 
         
