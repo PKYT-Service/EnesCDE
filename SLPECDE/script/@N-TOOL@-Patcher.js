8a3ab6f90c6d890a8347fffee35b782c7ca48119
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-04-28 12:51:15 +0200

    Update @N-TOOL@-Patcher.js

--- SLPECDE/script/@N-TOOL@-Patcher.js
+++ SLPECDE/script/@N-TOOL@-Patcher.js
@@ -15,7 +15,7 @@ function cleanString(input) {
   return input
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '') // Enleve les accents
-    .replace(/[^A-Za-z0-9@\-_/\. \n:(){}*_\`]/g, '') // Garde (){}[]**__` aussi
+    .replace(/[^A-Za-z0-9@\-_/\. \n:()\[\]{}*_\`]/g, '') // Garde (){}[]**__` aussi
     .replace(/[ ]{2,}/g, ' '); // Evite les doubles espaces
 }
 // Applique le nettoyage en temps reel sur les inputs et textareas
