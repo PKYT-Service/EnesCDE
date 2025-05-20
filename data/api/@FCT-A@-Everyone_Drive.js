902f434bba0b0090e10265d95555a9154d1b4ea3
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-05-21 00:31:16 +0200

    Update @FCT-A@-Everyone_Drive.js

--- data/api/@FCT-A@-Everyone_Drive.js
+++ data/api/@FCT-A@-Everyone_Drive.js
@@ -1159,5 +1159,3 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('resize', adaptLayoutForMobile);
 });
 
-import { ECDE_DriveStyle } from "./DriveStyle.js";
-ECDE_DriveStyle();
