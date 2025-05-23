94782a2f23a99f151d3d71f457e4eeae09becaab
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-05-23 22:18:32 +0200

    Update [PlugIn]-ProfilViewer.js

--- users/A-Service/[PlugIn]-ProfilViewer.js
+++ users/A-Service/[PlugIn]-ProfilViewer.js
@@ -72,29 +72,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
     </div>
 </div>
-<!-- Connected Toggle Buttons -->
-<h1 class="text-xl mt-4 text-blue-700 dark:text-blue-300">Profil</h1>
-<div class="flex flex-row items-center gap-2">
-    
-    <div class="flex flex-row">
-        <label class="toggle-button-label text-blue-600 dark:text-blue-400">
-            <input type="radio" name="radio-group-0" checked class="peer toggle-button rounded-l-full bg-blue-100 dark:bg-blue-900" />
-            <span class="toggle-button-indicator material-symbols-outlined text-blue-700 dark:text-blue-300">check</span>
-            <a href="" id="url_account:https://enes-cde.vercel.app/users/panel/profil.html"> consulter </a>
-        </label>
-        <label class="toggle-button-label text-blue-600 dark:text-blue-400">
-            <input type="radio" name="radio-group-0" class="peer toggle-button bg-blue-100 dark:bg-blue-900" />
-            <span class="toggle-button-indicator material-symbols-outlined text-blue-700 dark:text-blue-300">check</span>
-            <a href="" id="https://enes-cde.vercel.app/users/panel/edit_account.html"> modifier </a>
-        </label>
-        <label class="toggle-button-label text-blue-600 dark:text-blue-400">
-            <input type="radio" name="radio-group-0" class="peer toggle-button rounded-r-full bg-blue-100 dark:bg-blue-900" />
-            <span class="toggle-button-indicator material-symbols-outlined text-blue-700 dark:text-blue-300">check</span>
-            <a href="" id="https://enes-cde.vercel.app/users/panel/delete_account.html"> supprimer </a>
-        </label>
-    </div>
 
+
+<h1 class="text-xl mt-4 text-blue-700 dark:text-blue-300">Profil</h1>
+
+<div class="flex justify-center">
+    <nav
+        class="flex overflow-x-auto items-center p-1 space-x-1 rtl:space-x-reverse text-sm text-gray-600 bg-gray-500/5 rounded-xl dark:bg-gray-500/20">
+        <button role="tab" type="button"
+            class="flex whitespace-nowrap items-center h-8 px-5 font-medium rounded-lg outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-inset text-yellow-600 shadow bg-white dark:text-white dark:bg-yellow-600"
+            aria-selected="">
+            <a href="" id="url_account:https://enes-cde.vercel.app/users/panel/profil.html"> consulter </a>
+        </button>
+
+        <button role="tab" type="button"
+            class="flex whitespace-nowrap items-center h-8 px-5 font-medium rounded-lg outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-inset hover:text-gray-800 focus:text-yellow-600 dark:text-gray-400 dark:hover:text-gray-300 dark:focus:text-gray-400">
+            <a href="" id="https://enes-cde.vercel.app/users/panel/edit_account.html"> modifier </a>
+        </button>
+        <button role="tab" type="button"
+            class="flex whitespace-nowrap items-center h-8 px-5 font-medium rounded-lg outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-inset hover:text-gray-800 focus:text-yellow-600 dark:text-gray-400 dark:hover:text-gray-300 dark:focus:text-gray-400">
+            <a href="" id="https://enes-cde.vercel.app/users/panel/delete_account.html"> supprimer </a>
+        </button>
+    </nav>
 </div>
+
 `;
   });
 
