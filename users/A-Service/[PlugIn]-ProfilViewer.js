43c7d6e4bfb2d8a1ed868c802f7da094adef68f7
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-05-23 16:44:34 +0200

    Update [PlugIn]-ProfilViewer.js

--- users/A-Service/[PlugIn]-ProfilViewer.js
+++ users/A-Service/[PlugIn]-ProfilViewer.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         </span>
 
         <div class="w-full flex justify-center sm:justify-start sm:w-auto">
-            <img class="object-cover w-20 h-20 mt-3 mr-3 rounded-full" src="" id="COMPTE:ProfilInfo-Pdp">
+            <img class="object-cover mt-3 mr-3 rounded-full" style="height:20px;  width: 20px;" src="" id="COMPTE:ProfilInfo-Pdp">
         </div>
 
         <div class="w-full sm:w-auto flex flex-col items-center sm:items-start">
