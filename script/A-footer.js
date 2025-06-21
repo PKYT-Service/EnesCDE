0103cd52a2093812e594c1ce9bfa5c350f2e47f4
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-06-21 16:22:48 +0200

    Update A-footer.js

--- script/A-footer.js
+++ script/A-footer.js
@@ -12,13 +12,44 @@ document.addEventListener("DOMContentLoaded", function () {
             </div>
         </div>
         <p class="mt-5 text-center text-sm leading-6 text-slate-500 dark:text-gray-400">
-            © <a id="ecde_annee_set">0000</a> Enes - CDE. Tous droits réservés.
+            
         </p>
         <div class="mt-8 flex items-center justify-center space-x-4 text-sm font-semibold leading-6 text-slate-700 dark:text-gray-300">
             <a href="https://enes-cde.vercel.app/redirect.html?LB=pdc">Politique de confidentialité</a>
             <div class="h-4 w-px bg-slate-500/20 dark:bg-gray-600"></div>
             <a href="https://enes-cde.vercel.app/redirect.html?LB=discord">Discord</a>
         </div>
+    `;
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("ecde_footer").innerHTML = `
+     <br><br>
+<footer class="footer bg-base-200/60 px-6 py-4">
+  <div class="flex w-full flex-wrap items-center justify-between">
+    <div class="flex items-center gap-2 text-xl font-bold text-base-content">
+      <img src="https://enes-cde.vercel.app/data/img/web/favicon.png" style="border-radius: 10px; width: 24px; height: 24px;"
+      <span>E-CDE</span>
+    </div>
+    <aside class="grid-flow-col items-center">
+      <p> © <a id="ecde_annee_set">0000</a> Enes - <a class="text-blue-800"> CDE</a>. Tous droits réservés.  </p>
+    </aside>
+    <div class="flex h-5 gap-4">
+      <a href="#" class="link" aria-label="Github Link">
+        <span class="icon-[tabler--brand-github] size-5"></span>
+      </a>
+      <a href="#" class="link" aria-label="Facebook Link">
+        <span class="icon-[tabler--brand-facebook] size-5"></span>
+      </a>
+      <a href="#" class="link" aria-label="X Link">
+        <span class="icon-[tabler--brand-x] size-5"></span>
+      </a>
+      <a href="#" class="link" aria-label="Google Link">
+        <span class="icon-[tabler--brand-google] size-5"></span>
+      </a>
+    </div>
+  </div>
+</footer>
     `;
   });
 
