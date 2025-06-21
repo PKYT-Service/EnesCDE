167828159126256f810291ca66f07cedc08f0f85
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-06-21 17:14:23 +0200

    Update A-footer.js

--- script/A-footer.js
+++ script/A-footer.js
@@ -24,30 +24,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("ecde_footer").innerHTML = `
-     <br><br>
-<footer class="footer dark:bg-gray-900 bg-white px-6 py-4" style="border-top-left-radius:15px; border-top-right-radius:15px;>
-  <div class="flex w-full flex-wrap items-center justify-between">
-    <div class="flex items-center gap-2 text-xl font-bold text-base-content">
+<footer class="footer dark:bg-gray-900 bg-white  flex flex-col items-center gap-4 p-6" style="border-top-left-radius:15px; border-top-right-radius:15px;>
+  <div class="flex items-center gap-2 text-xl font-bold text-base-content">
       <img src="https://enes-cde.vercel.app/data/img/web/favicon.png" style="border-radius: 10px; width: 24px; height: 24px;"
       <span>E-CDE</span>
-    </div>
-    <aside class="grid-flow-col items-center">
-      <p> © <a id="ecde_annee_set">0000</a> Enes - <a class="text-blue-800"> CDE</a>. Tous droits réservés.  </p>
-    </aside>
-    <div class="flex h-5 gap-4">
-      <a href="#" class="link" aria-label="Github Link">
-        <span class="icon-[tabler--brand-github] size-5"></span>
-      </a>
-      <a href="#" class="link" aria-label="Facebook Link">
-        <span class="icon-[tabler--brand-facebook] size-5"></span>
-      </a>
-      <a href="#" class="link" aria-label="X Link">
-        <span class="icon-[tabler--brand-x] size-5"></span>
-      </a>
-      <a href="#" class="link" aria-label="Google Link">
-        <span class="icon-[tabler--brand-google] size-5"></span>
-      </a>
-    </div>
+  </div>
+  <aside>
+    <p>© <a id="ecde_annee_set">0000</a> <b>Enes - <a class="text-blue-800"> CDE</a></b>. Tous droits réservés.</p>
+  </aside>
+  <nav class="grid-flow-col gap-4">
+    <a class="link link-hover text-base-content" href=""https://enes-cde.vercel.app/equipe.html>équipe</a>
+    <a class="link link-hover text-base-content" href="#dev">Help</a>
+    <a class="link link-hover text-base-content" href="https://enes-cde.vercel.app/redirect.html?LB=pdc">Politique de confidentialité</a>
+    <a class="link link-hover text-base-content" href="https://enes-cde.vercel.app/redirect.html?LB=discord">Discord</a>
+  </nav>
+  <div class="flex h-5 gap-4">
+    <a href="https://github.com/PKYT-Service/" class="link text-base-content" aria-label="Github Link">
+      <span class="icon-[tabler--brand-github-filled] size-5"></span>
+    </a>
+    <a href="https://enes-cde.vercel.app/" class="link text-base-content" aria-label="Google Link">
+      <span class="icon-[tabler--brand-google-filled] size-5"></span>
+    </a>
   </div>
 </footer>
     `;
