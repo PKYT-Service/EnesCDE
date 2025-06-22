ebc9d93c3c4ba55c7beb96e1a9874860d385671a
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-06-22 14:37:33 +0200

    Update @N-Check@-StoreLogin.js

--- SLPECDE/script/@N-Check@-StoreLogin.js
+++ SLPECDE/script/@N-Check@-StoreLogin.js
@@ -1,4 +1,4 @@
-export function storeLoginFromURL() {
+function storeLoginFromURL() {
   try {
     const params = new URLSearchParams(window.location.search);
     const rawAcc = params.get("acc");
