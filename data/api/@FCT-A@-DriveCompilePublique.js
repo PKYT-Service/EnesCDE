b043fa323823b9dde23e7c13198203f16986440b
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-07-05 00:18:16 +0200

    Update @FCT-A@-DriveCompilePublique.js

--- data/api/@FCT-A@-DriveCompilePublique.js
+++ data/api/@FCT-A@-DriveCompilePublique.js
@@ -165,3 +165,19 @@ document.write(`
 </section>
           <script src="https://enes-cde.vercel.app/data/api/@FCT-A@-DriveScript.js" defer></script>
 `);
+
+  const fileViewSection = document.getElementById('file-view-section');
+  const btnCloseView = document.getElementById('btn-close-view');
+  const fileUl = document.getElementById('files-ul');
+
+  // Quand un fichier est clique (attente du contenu charge)
+  fileUl.addEventListener('click', (e) => {
+    if (e.target.closest('li')) {
+      fileViewSection.classList.add('open');
+    }
+  });
+
+  // Bouton pour fermer
+  btnCloseView.addEventListener('click', () => {
+    fileViewSection.classList.remove('open');
+  }); 
