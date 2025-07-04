270ae80d5a5698b556f58aa747afd5e10f4d558f
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-07-05 00:29:52 +0200

    Update @FCT-A@-DriveCompilePublique.js

--- data/api/@FCT-A@-DriveCompilePublique.js
+++ data/api/@FCT-A@-DriveCompilePublique.js
@@ -166,18 +166,18 @@ document.write(`
           <script src="https://enes-cde.vercel.app/data/api/@FCT-A@-DriveScript.js" defer></script>
 `);
 
-  const fileViewSection = document.getElementById('file-view-section');
-  const btnCloseView = document.getElementById('btn-close-view');
-  const fileUl = document.getElementById('files-ul');
-
-  // Quand un fichier est clique (attente du contenu charge)
-  fileUl.addEventListener('click', (e) => {
-    if (e.target.closest('li')) {
-      fileViewSection.classList.add('open');
-    }
-  });
-
-  // Bouton pour fermer
-  btnCloseView.addEventListener('click', () => {
-    fileViewSection.classList.remove('open');
-  }); 
+  //  const fileViewSection = document.getElementById('file-view-section');
+  //  const btnCloseView = document.getElementById('btn-close-view');
+  //  const fileUl = document.getElementById('files-ul');
+  //
+  //  // Quand un fichier est clique (attente du contenu charge)
+  //  fileUl.addEventListener('click', (e) => {
+  //    if (e.target.closest('li')) {
+  //      fileViewSection.classList.add('open');
+  //    }
+  //  });
+  //
+  //  // Bouton pour fermer
+  //  btnCloseView.addEventListener('click', () => {
+  //    fileViewSection.classList.remove('open');
+  //  }); 
