6a40c4ea0939cc3528db880830290059f6327fce
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-08-25 23:48:22 +0200

    Delete data/MID_MC/launch.bat

--- data/MID_MC/launch.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\e-cde\MID"
-echo.
-echo Lancement du gestionnaire MID...
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass; ./launch.ps1"
-pause
